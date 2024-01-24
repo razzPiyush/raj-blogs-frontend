e7d9e751722846ff22e5c39c33dfaf3ce099cec2
Piyush Raj <106405216+razzPiyush@users.noreply.github.com>
2024-01-24 23:53:57 +0530

    Update BlogDetail.js

--- src/components/BlogDetail.js
+++ src/components/BlogDetail.js
@@ -19,7 +19,7 @@ const BlogDetail = () => {
   };
   const fetchDetails = async () => {
     const res = await axios
-      .get(`http://raj-blogs.vercel.app/api/blog/${id}`)
+      .get(`http://raj-blogs.onrender.com/api/blog/${id}`)
       .catch((err) => console.log(err));
     const data = await res.data;
     return data;
